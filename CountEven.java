90cb0171775b3936dff752baaacba7459b04742c
miruthula <miruthula183@gmail.com>
2026-07-11 09:44:53 -0700

    Added CountEven program using for-each loop

---
+++ CountEven.java
@@ -0,0 +1,19 @@
+public class CountEven {
+    public static void main(String[] args) {
+
+        int[] arr = {2,5,8,11,14,19};
+
+        int count = 0;
+
+        for(int x : arr){
+
+            if(x % 2 == 0){
+                count++;
+            }
+
+        }
+
+        System.out.println("Even Count = " + count);
+
+    }
+}
